6c2095c463722d945843b1577839930d9aabebac
bijayv <nrbijay52@gmail.com>
2024-07-14 15:51:08 +0530

    Update StringCalculatorTest.py

--- StringCalculatorTest.py
+++ StringCalculatorTest.py
@@ -18,10 +18,9 @@ class TestStringCalculator(unittest.TestCase):
                 
     def test_expectSumWithNewlineDelimiter(self):
         self.assertEqual(add("1\n2,3"),6);
-    def test_negative(self):
-        add("-1, -2, -3, 1, 2, 3")
-    def test_negative_numbers_exception():
-	add('-1, -2, -3, 1, 2, 3')
+	    
+    def test_negative_numbers_exception(self):
+	add("-1, -2, -3, 1, 2, 3")
 	assert(Exception == 'Negatives No not allowed \[-1, -2, -3\]')
 	
         
