e1aa4811dda35e07f921cfd8e58b3afd9023cd1c
bijayv <nrbijay52@gmail.com>
2024-07-14 14:09:43 +0530

    Update StringCalculatorTest.py
    Adding testcase for checking negative numbers

--- StringCalculatorTest.py
+++ StringCalculatorTest.py
@@ -1,23 +1,27 @@
 import unittest
-from StringCalculator import add1
+from StringCalculator import add
 class TestStringCalculator(unittest.TestCase):
     def test_add_returns_zero_for_emptyString(self):
-        self.assertEqual(add1(""), 0)
-        self.assertEqual(add1("0"), 0)
+        self.assertEqual(add(""), 0)
                 
     def test_add_returns_zero_for_singleString(self):
-        self.assertEqual(add1("0"), 0)
+        self.assertEqual(add("0"), 0)
                 
     def test_expectSumForTwoNumberst(self):
-        self.assertEqual(add1("1,2"), 3)
+        self.assertEqual(add("1,2"), 3)
                 
     def test_ignoreNumbersGreaterThan1000(self):
-        self.assertEqual(add1("1,1001"), 1)
+        self.assertEqual(add("1,1001"), 1)
                 
     def test_expectSumWithCustomDelimiter(self):
-        self.assertEqual(add1("//;\n1;2"), 3)
+        self.assertEqual(add("//;\n1;2"), 3)
                 
     def test_expectSumWithNewlineDelimiter(self):
-        self.assertEqual(add1("1\n2,3"),6);
+        self.assertEqual(add("1\n2,3"),6);
+    try:
+        def test_negative(self):
+            add("-1, -2, -3, 1, 2, 3")
+    except Exception as exception:
+        assert str(exception) == "Negative No not allowed [-1, -2, -3]"
 if __name__ == '__main__':
     unittest.main()
