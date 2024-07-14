15185fb380b455fa56158717a0b09703c3d451a6
bijayv <nrbijay52@gmail.com>
2024-07-14 15:52:29 +0530

    Update StringCalculatorTest.py

--- StringCalculatorTest.py
+++ StringCalculatorTest.py
@@ -20,8 +20,8 @@ class TestStringCalculator(unittest.TestCase):
         self.assertEqual(add("1\n2,3"),6);
 	    
     def test_negative_numbers_exception(self):
-	add("-1, -2, -3, 1, 2, 3")
-	assert(Exception == 'Negatives No not allowed \[-1, -2, -3\]')
+	    add("-1, -2, -3, 1, 2, 3")
+		assert(Exception == 'Negatives No not allowed \[-1, -2, -3\]')
 	
         
 if __name__ == '__main__':
