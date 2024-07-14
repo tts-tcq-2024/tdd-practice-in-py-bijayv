cdb08349ad16f15edf50650c9b502b62de1b82a1
bijayv <nrbijay52@gmail.com>
2024-07-14 13:51:25 +0530

    Update StringCalculatorTest.py

--- StringCalculatorTest.py
+++ StringCalculatorTest.py
@@ -5,19 +5,19 @@ class TestStringCalculator(unittest.TestCase):
         self.assertEqual(add1(""), 0)
         self.assertEqual(add1("0"), 0)
                 
-        #def expectZeroForSingleZero(self):
-                #self.assertEqual(add("0"), 0)
+    def expectZeroForSingleZero(self):
+        self.assertEqual(add("0"), 0)
                 
-        #def expectSumForTwoNumberst(self):
-                #self.assertEqual(add("1,2"), 3)
+    def expectSumForTwoNumberst(self):
+        self.assertEqual(add("1,2"), 3)
                 
-        #def ignoreNumbersGreaterThan1000(self):
-                #self.assertEqual(add("1,1001"), 1)
+    def ignoreNumbersGreaterThan1000(self):
+        self.assertEqual(add("1,1001"), 1)
                 
-        #def expectSumWithCustomDelimiter(self):
-                #self.assertEqual(add("//;\n1;2"), 3)
+    def expectSumWithCustomDelimiter(self):
+        self.assertEqual(add("//;\n1;2"), 3)
                 
-        #def expectSumWithNewlineDelimiter(self):
-                #self.assertEqual(add("1\n2,3"),6);
+    def expectSumWithNewlineDelimiter(self):
+        self.assertEqual(add("1\n2,3"),6);
 if __name__ == '__main__':
     unittest.main()
